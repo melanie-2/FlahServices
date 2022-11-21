3a530b7cf62fe1c475fce0569543b3628f5f9d5e
melanie-2 <117998380+melanie-2@users.noreply.github.com>
2022-11-21 14:39:45 -0300

    old
    old

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+let button1 = document.getElementById("button1");
+let button2 = document.getElementById("button2");
+
+function evento1(){
+    button1.onclick = alert("Este é apenas um exemplo");
+}
+
+function evento2(){
+    button2.onclick = alert("Este é apenas mais um exemplo");
+}
